45c2ce6844ac42bb9176a331fdf1d79c7611b92d
spootmu <spoot@qq.com>
2016-01-11 16:42:55 +0800

    .

--- CustomConfigIOS/Control/ViewNewsViewController.m
+++ CustomConfigIOS/Control/ViewNewsViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewNewsViewController.h"
 
-@interface ViewNewsViewController ()
+@interface ViewNewsViewController ()<UIWebViewDelegate>
 @property(weak,nonatomic)UIWebView *webpage;
 @end
 
@@ -21,6 +21,25 @@
     [self setupUI];
 }
 
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+//    [webView stringByEvaluatingJavaScriptFromString:
+//     @"var script = document.createElement('script');"
+//     "script.type = 'text/javascript';"
+//     "script.text = \"function ResizeImages() { "
+//     "var myimg,oldwidth;"
+//     "var maxwidth = 375.0;" // UIWebView中显示的图片宽度
+//     "for(i=0;i <document.images.length;i++){"
+//     "myimg = document.images[i];"
+//     "if(myimg.width > maxwidth){"
+//     "oldwidth = myimg.width;"
+//     "myimg.width = maxwidth;"
+//     "}"
+//     "}"
+//     "}\";"
+//     "document.getElementsByTagName('head')[0].appendChild(script);"];
+//    [webView stringByEvaluatingJavaScriptFromString:@"ResizeImages();"];
+}
 
 -(void)setupUI
 {
@@ -29,8 +48,13 @@
     NSURLRequest *url=[NSURLRequest requestWithURL:[NSURL URLWithString:urls]];
     
     UIWebView *webpage=[[UIWebView alloc]init];
+//    webpage.scalesPageToFit=YES;
+    webpage.delegate=self;
     webpage.frame=CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
     [webpage loadRequest:url];
+    
+    
+    
     [self.view addSubview:webpage];
     self.webpage=webpage;
 }
